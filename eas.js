9e497c0e8cf86c05b1d9ee175d0811b36852ec01
Andrew Marabante <andrewmarabante@gmail.com>
2023-06-23 17:25:44 -0700

    Last grid was 4x4, created an actual 16x16.... lol Used display grid this time to allow for scaling.

--- eas.js
+++ eas.js
@@ -1,28 +1,6 @@
 const grid = document.querySelector('.grid');
-const r1 = document.querySelector('.row1');
-const r2 = document.querySelector('.row2');
-const r3 = document.querySelector('.row3');
-const r4 = document.querySelector('.row4');
-for(i=0;i<=15;i++)
+for(i=0;i<=255;i++)
     {
-        if(i>=0 && i<=3)
-        {
-            const row1 = document.createElement('DIV')
-            r1.appendChild(row1)
-        }
-        if(i>=4 && i<=7)
-        {
-            const row2 = document.createElement('DIV')
-            r2.appendChild(row2)
-        }
-        if(i>=8 && i<=11)
-        {
-            const row3 = document.createElement('DIV')
-            r3.appendChild(row3)
-        }
-        if(i>=12 && i<=15)
-        {
-            const row4 = document.createElement('DIV')
-            r4.appendChild(row4)
-        }
+        const gridblock = document.createElement('DIV')
+        grid.appendChild(gridblock);
     }
