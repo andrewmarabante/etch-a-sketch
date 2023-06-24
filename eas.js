10b37f59b24375ff1fa7797f9d3c1a47eeb943ff
Andrew Marabante <andrewmarabante@gmail.com>
2023-06-23 18:54:10 -0700

    Added a click listener so you need to hover AND click to change the color.

--- eas.js
+++ eas.js
@@ -1,10 +1,20 @@
 const grid = document.querySelector('.grid');
-for(i=0;i<=255;i++)
-    {
+for(i=0;i<=255;i++){
         const gridblock = document.createElement('DIV')
         gridblock.addEventListener('mouseover',changeColor)
+        gridblock.addEventListener('mousedown',changeColor)
         grid.appendChild(gridblock);
     }
-function changeColor(e){
+
+
+let click = false;
+document.onmousedown = () => {click = true};
+document.onmouseup = () => {click = false};
+
+function changeColor(e)
+{
+    if(e.type === 'mouseover' && !click) return
+    else if(e.type === 'mouseover' && click){
     e.target.style.backgroundColor = 'black';
+    }
 }
