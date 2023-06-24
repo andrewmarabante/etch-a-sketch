b370e5052f62679a3c78b0a767f1310236f20344
Andrew Marabante <andrewmarabante@gmail.com>
2023-06-23 18:24:09 -0700

    Added an eventlistener to each gridblock and a changcolor function

--- eas.js
+++ eas.js
@@ -2,5 +2,9 @@ const grid = document.querySelector('.grid');
 for(i=0;i<=255;i++)
     {
         const gridblock = document.createElement('DIV')
+        gridblock.addEventListener('mouseover',changeColor)
         grid.appendChild(gridblock);
     }
+function changeColor(e){
+    e.target.style.backgroundColor = 'black';
+}
